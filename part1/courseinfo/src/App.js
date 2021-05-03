155811af9809833df53a980d78755ae2083c0f10
Chris Reed <cjr6439@gmail.com>
2021-05-02 21:56:16 -0500

    Added ex 1.1, 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+
+const Header = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>{props.course}</h1>
+  )
+}
+const Part = (props) => {
+  return (
+    <p>{props.partname} {props.num_exercises}</p>
+  )
 }
 
-export default App;
+const Total = (props) => {
+
+  return (
+    <p>Number of exercises { props.num_exercises1 + props.num_exercises2 + props.num_exercises3}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+
+      <Part partname={part1} num_exercises={exercises1} />
+      <Part partname={part2} num_exercises={exercises2} />
+      <Part partname={part3} num_exercises={exercises3} />
+
+      <Total num_exercises1={exercises1} num_exercises2={exercises2} num_exercises3={exercises3} />
+    </div>
+  )
+}
+
+export default App
